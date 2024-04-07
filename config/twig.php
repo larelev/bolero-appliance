7119d86c08c34b0571a7dc6b919652931febdbbe
David Blanchard <dpjb@hotmail.fr>
2024-04-07 15:37:21 +0200

    Fix: flash message view is now part of the plugin.

--- config/twig.php
+++ config/twig.php
@@ -3,4 +3,5 @@
 return [
     \Bolero\Plugins\Authentication\Configuration::viewsPath(),
     \Bolero\Plugins\Dashboard\Configuration::viewsPath(),
+    \Bolero\Plugins\FlashMessage\Configuration::viewsPath(),
 ];
